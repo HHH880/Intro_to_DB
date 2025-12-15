b366c976cf3bd6aa1c6221cf05570706ea952afd
Elvis_Kusi <135480109+HHH880@users.noreply.github.com>
2025-12-15 06:48:06 -0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- CUSTOMERS TABLE (CASE-SENSITIVE)
+-- CUSTOMERS TABLE
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -28,8 +28,8 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
--- ORDERS TABLE
-CREATE TABLE IF NOT EXISTS orders (
+-- ORDERS TABLE (CASE-SENSITIVE)
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
@@ -37,11 +37,11 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- ORDER DETAILS TABLE
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
